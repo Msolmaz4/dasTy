092b73556a8e57cf8c00192638f9b0327e8ca21e
mustafa <msolmaz83@gmail.com>
2023-12-13 14:34:29 +0100

    dasboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,36 +1,60 @@
-import Sigin from './pages/Sigin'
-import Signup from './pages/Signup'
-import Eroro from './pages/Eroro'
-import Stock from './pages/Stock';
-
-
-
-
-
+import Sigin from "./pages/Sigin";
+import Signup from "./pages/Signup";
+import Eroro from "./pages/Eroro";
+import Home from "./pages/Home";
+import Sales from "./pages/Sales";
+import Stock from "./pages/Stock";
+import Firms from "./pages/Firms";
+import Brands from "./pages/Brands";
+import Products from "./pages/Products";
+import Purchases from "./pages/Purchases";
 
 const routes = [
-    {
-        path:'/',
-        element:<Sigin/>
-    },
-    {
-        path:'/signup',
-        element:<Signup/>
-    },
+  {
+    path: "/",
+    element: <Sigin />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
 
-    {
-        path:'/stock',
-        element:<Stock/>
-    },
-    {
-        path:'*',
-        element:<Eroro/>
-    },
-   
-    
-  
+  {
+    path: "stock",
+    element: <Stock/>,
+    auth: false,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
 
-    
-]
+      {
+        path: "sales",
+        element: <Sales />,
+      },
+      {
+        path: "firms",
+        element: <Firms />,
+      },
+      {
+        path: "brands",
+        element: <Brands />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "purchases",
+        element: <Purchases />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Eroro />,
+  },
+];
 
 export default routes;
